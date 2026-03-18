30f86ca255df4fafe5406d6d424811c95de64acb
Igihozo Belise <igihozobelise6@gmail.com>
2026-03-18 20:53:27 +0200

    Fix image path for Vercel

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,33 +1,34 @@
-import ai from '../../images/ai.png'
-import contribution from '../../images/contribution.png'
-import group from '../../images/group.png'
-import hands from '../../images/hands.jpg'
-import multilingual from'../../images/multilingual.png'
-import notification from '../../images/notification.png'
-import offline from '../../images/offline.png'
-import pair from '../../images/pair.png'
-import timer from '../../images/timer.png'
-import ussd from '../../images/ussd.png'
-import safiLogo from '../../images/safiLogo.png'
-import Footprints from '../../images/footprints.png'
-import Arrow from '../../images/arrow.png'
-import Union from '../../images/union.png'
-import PeopleOnCircle from '../../images/peopleOnCircle.png'
+// src/constants/images.js
+import ai from '@/images/ai.png';
+import contribution from '@/images/contribution.png';
+import group from '@/images/group.png';
+import hands from '@/images/hands.jpg';
+import multilingual from '@/images/multilingual.png';
+import notification from '@/images/notification.png';
+import offline from '@/images/offline.png';
+import pair from '@/images/pair.png';
+import timer from '@/images/timer.png';
+import ussd from '@/images/ussd.png';
+import safiLogo from '@/images/safiLogo.png';
+import Footprints from '@/images/footprints.png';
+import Arrow from '@/images/arrow.png';
+import Union from '@/images/union.png';
+import PeopleOnCircle from '@/images/peopleOnCircle.png';
 
 export const image = {
-    ai,
-    contribution,
-    Footprints,
-    Arrow,
-    group,
-    hands,
-    multilingual,
-    notification,
-    offline,
-    pair,
-    Union,
-    PeopleOnCircle,
-    timer,
-    ussd,
-    safiLogo
-}
+  ai,
+  contribution,
+  Footprints,
+  Arrow,
+  group,
+  hands,
+  multilingual,
+  notification,
+  offline,
+  pair,
+  Union,
+  PeopleOnCircle,
+  timer,
+  ussd,
+  safiLogo
+};
